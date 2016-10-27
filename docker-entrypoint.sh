238dc1595693e627c88e8ac92a2319524c1be7a2
Karl Newell <karlnewell@gmail.com>
2016-10-27 12:03:09 -0700

    Update docker-entrypoint.sh to initialize DB only if it doesn't exist

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
-service mysql start
-/usr/bin/mysql -e 'CREATE DATABASE IF NOT EXISTS fod'
+if [ ! -d "/var/lib/mysql/mysql" ]; then #check whether the DB is initialized.
+    echo 'Initializing database'
+    mysql_install_db
+    echo 'Database initialized'
+    
+    service mysql start
+    /usr/bin/mysql -e 'CREATE DATABASE IF NOT EXISTS fod'
+else
+    service mysql start
+fi
 
 service memcached start
 service beanstalkd start
